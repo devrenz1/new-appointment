dd0960affeefa77df88b795624d62042c9222eb1
renz <renzuye18@gmail.com>
2023-08-07 10:34:07 +0800

    push

---
+++ resources/views/layout/book.blade.php
@@ -0,0 +1,20 @@
+
+<section class="cta">
+    <div class="cta-content">
+        <div class="container px-5">
+            <h2 class="text-white display-1 lh-1 mb-4">
+                Stop waiting.
+                <br />
+                Start building.
+            </h2>
+            {{-- <button id="bottone5"  target="_blank">Book Appointment</button> --}}
+            <button id="bottone5" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                <span class="d-flex align-items-center">
+                 
+                    <span class="small">Book Appointment</span>
+                </span>
+            </button>
+        </div>
+    </div>
+    
+</section>
---
+++ resources/views/lp/dash.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.app')
+@include('layout.book')
+@include('lp.appointment')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Appointment</title>
+    <link href="{{asset('/css/layout.css')}}" rel="stylesheet" >
+
+</head>
+@section('contents')
+<body>
+  
+  
+    
+
+</body>
+
+
+</html>
+@endsection
